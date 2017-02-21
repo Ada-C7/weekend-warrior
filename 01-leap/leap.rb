de8ca1bc78f6c9beff56b3f8a9dc06150b8b2f0e
Janice Lichtman <jalichtman@gmail.com>
2017-02-21 10:39:27 -0800

    No need for 'false' for non-leap years'

--- 01-leap/leap.rb
+++ 01-leap/leap.rb
@@ -14,8 +14,7 @@
 
 
 def leap_year?(user_year)
-  leap_year = false
-
+  #leap_year = false
   if user_year % 4 == 0 && (!(user_year % 100 == 0) || (user_year % 100 == 0 && user_year % 400 == 0))
     leap_year = true
   end
